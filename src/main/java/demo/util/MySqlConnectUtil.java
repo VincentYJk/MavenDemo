63fec03874a5a40bca8178c57e1e39e66cd63fef
梁明辉 <1723153306.qq.com>
2019-03-01 16:48:42 +0800

    修改

--- src/main/java/demo/util/MySqlConnectUtil.java
+++ src/main/java/demo/util/MySqlConnectUtil.java
@@ -5,15 +5,15 @@ import java.sql.*;
 public class MySqlConnectUtil {
     // JDBC 驱动名及数据库 URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://119.191.58.20:3308/website_yantai?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+    static final String DB_URL = "";
 
     // 数据库的用户名与密码，需要根据自己的设置
-    static final String USER = "website";
-    static final String PASS = "websi173te";
+    static final String USER = "";
+    static final String PASS = "";
     public static Connection getConnect()throws Exception{
         Connection conn = null;
         // 注册 JDBC 驱动
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName(JDBC_DRIVER);
        return  DriverManager.getConnection(DB_URL, USER, PASS);
     }
 }
