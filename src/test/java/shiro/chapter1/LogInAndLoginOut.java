f6c8287fa3237905a617067169a8679b628e8804
梁明辉 <1723153306@qq.com>
2019-05-23 17:01:48 +0800

    身份验证完成

---
+++ src/test/java/shiro/chapter1/LogInAndLoginOut.java
@@ -0,0 +1,64 @@
+package shiro.chapter1;
+
+import org.apache.catalina.security.SecurityUtil;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @ClassName LogInAndLoginOut
+ * @Description 身份验证, 登录退出案例
+ * @Author 梁明辉
+ * @Date 2019/5/22 14:27
+ * @ModifyDate 2019/5/22 14:27
+ * @Version 1.0
+ */
+public class LogInAndLoginOut {
+    @Test
+    public void test() {
+        Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:config/shiro-realm.ini");
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+        }
+        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+        //6、退出
+        subject.logout();
+    }
+
+    public void login(String configFile) {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<org.apache.shiro.mgt.SecurityManager> factory =
+                new IniSecurityManagerFactory(configFile);
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        subject.login(token);
+    }
+
+    @Test
+    public void testLogin() {
+        login("classpath:config/shiro-authenticator-all-success.ini");
+        Subject subject = SecurityUtils.getSubject();
+        //得到一个身份集合，其包含了Realm验证成功的身份信息
+        PrincipalCollection principalCollection = subject.getPrincipals();
+        Assert.assertEquals(1, principalCollection.asList().size());
+    }
+}
---
+++ src/test/java/shiro/chapter1/MyRealm1.java
@@ -0,0 +1,44 @@
+package shiro.chapter1;
+
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * 来源:https://jinnianshilongnian.iteye.com/category/305053
+ * @ClassName MyRealm1
+ * @Author 梁明辉
+ * @Date 2019/5/22 15:35
+ * @ModifyDate 2019/5/22 15:35
+ * @Version 1.0
+ */
+public class MyRealm1 implements Realm {
+    @Override
+    public String getName() {
+        return "myRealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) {
+        String username = (String) token.getPrincipal();
+        String password = new String((char[]) token.getCredentials());
+        if (!"zhang".equals(username)) {
+            //未知用户异常
+            throw new UnknownAccountException();
+        }
+        if (!"123".equals(password)) {
+            throw new IncorrectCredentialsException();
+        }
+        //验证通过返回一个有效实现
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
